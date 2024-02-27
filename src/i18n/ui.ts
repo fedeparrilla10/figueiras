8a0dcf3d771bbae8cb5c68ea53b77a274d73a58f
fedeparrilla10 <federicoivanpd@gmail.com>
2024-02-27 22:48:58 +0200

    [add] index img + titles

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,6 +15,10 @@ export const ui = {
     'nav.alameda23': 'Casa Alameda 23',
     'nav.alameda33': 'Casa Alameda 33',
     'nav.contact': 'Contacto',
+
+    'header.welcome': 'Bienvenido a',
+    'header.title': 'Casas das Figueiras',
+    'header.subtitle': 'Alquiler de casas vacacionales',
   },
   en: {
     'nav.home': '',
@@ -22,6 +26,10 @@ export const ui = {
     'nav.alameda23': '',
     'nav.alameda33': '',
     'nav.contact': 'Contact',
+
+    'header.welcome': 'Welcome to',
+    'header.title': 'Casas das Figueiras',
+    'header.subtitle': 'Holiday homes for rent',
   },
   de: {
     'nav.home': '',
@@ -29,5 +37,9 @@ export const ui = {
     'nav.alameda23': '',
     'nav.alameda33': '',
     'nav.contact': 'Kontakt',
+
+    'header.welcome': 'Willkommen bei',
+    'header.title': 'Casas das Figueiras',
+    'header.subtitle': 'Ferienhäuser zu vermieten',
   },
 } as const;
